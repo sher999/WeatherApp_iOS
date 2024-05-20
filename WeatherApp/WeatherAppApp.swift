c99b733d362b63cc09f1ff95e7905d69f04b6911
shreyashd999 <shreyashdewangan1234@gmail.com>
2024-05-20 17:41:13 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Shreyash Dewangan on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
